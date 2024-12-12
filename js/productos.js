1f5b840abb9185d157aaa891ae456021ad8a02ec
Andrepg25 <114041137+Andrepg25@users.noreply.github.com>
2024-12-12 18:56:33 -0500

    precio organizado

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
 const productos = [
-    {id:1, nombre: 'monitor lg 24 pulgas', precio: 100, imagen: 'img/lgmonitor.webp' },
-    {id:2, nombre: 'Monitor gamer curvo Samsung LED ', precio: 150, imagen: 'img/monitorcurvo.jpeg' },
+    {id:1, nombre: 'monitor lg 24 pulgas', precio: 679000, imagen: 'img/lgmonitor.webp' },
+    {id:2, nombre: 'Monitor gamer curvo Samsung LED ', precio: 1079000, imagen: 'img/monitorcurvo.jpeg' },
     {id:3, nombre: 'Teclado Gamer Xtrike Me Mecanico ', precio: 200, imagen: 'img/tecladorainbow.webp' },
     {id:4, nombre: 'logitech G-33', precio: 250, imagen: 'img/logitechG33.webp' },
     {id:5, nombre: 'logitech G-304', precio: 300, imagen: 'img/logitechG304.webp' },
